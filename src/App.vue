936085523b07c233f745320822ebed43d1a89de5
Oguzhan <oguzhanmemis266@gmail.com>
2021-12-30 14:37:48 +0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
-<template>  
-    <router-link to="/" active-class="active">
-      Home
-    </router-link>
-    <router-link to="/about" active-class="active">
-      About
-    </router-link>
-    <router-link to="/details/123" active-class="active">
-      Details
-    </router-link>
-  <router-view></router-view>
+<template>
+  <div id="app">
+    <router-link to="/" active-class="active"> Home </router-link>
+    <router-link to="/about" active-class="active"> About </router-link>
+    <router-link to="/details/123" active-class="active"> Details </router-link>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-  .active{
-    color: red;
-  }
+#app {
+  
+}
+
+.active {
+  color: red;
+}
 </style>
